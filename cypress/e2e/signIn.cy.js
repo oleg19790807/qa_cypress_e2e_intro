719c9cb05f4d1e8e2c57ef3103f7eec3c728d02a
олег иванов <you@example.com>
2025-04-23 13:53:00 +0200

    solve

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
-
-  });
+Cypress.Commands.add('login', (email, password) => {
+  cy.visit('https://react-redux.realworld.io/#/login');
+  cy.get('input[type="email"]').type(email);
+  cy.get('input[type="password"]').type(password);
+  cy.get('button[type="submit"]').click();
 });
